6fa499237c50d462f530325f300a3488187dfcc3
kashifashah1 <kashifa.shah24@gmail.com>
2026-05-08 18:06:24 +0530

    Added routes in the Project

---
+++ client/src/pages/AppLayout.tsx
@@ -0,0 +1,17 @@
+import { Outlet } from "react-router-dom";
+
+const AppLayout = () => {
+  return (
+    <>
+      <p>banner</p>
+      <p>navbar</p>
+      <main className="min-h-screen">
+        <Outlet />
+      </main>
+      <p>footer</p>
+      <p>card sidebar</p>
+    </>
+  );
+};
+
+export default AppLayout;
