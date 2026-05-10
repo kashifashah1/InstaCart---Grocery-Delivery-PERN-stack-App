9121e2e493eef80f77b487da610a6e5f637005f1
kashifashah1 <kashifa.shah24@gmail.com>
2026-05-10 19:33:02 +0530

    Work in progress in the login form

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,8 +1,72 @@
+import { useState } from "react";
+import { heroSectionData } from "../assets/assets";
+import { Link } from "react-router-dom";
+import { BikeIcon } from "lucide-react";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [isLoginState, setIsLoginState] = useState(true);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
 
-export default Login
+  const handleSubmit = async (e: React.SubmitEvent) => {
+    e.preventDefault();
+    setLoading(true);
+    setTimeout(() => (window.location.href = "/"), 1000);
+  };
+
+  return (
+    <div className="min-h-screen flex">
+      {/* Left Side */}
+
+      <div className="hidden lg:flex lg:w-1/2 bg-app-green relative items-center justify-center">
+        <img
+          src={heroSectionData.hero_image}
+          alt=""
+          className="absolute inset-0 object-cover h-full bg-center opacity-10"
+        />
+
+        <div className="relative text-center px-12">
+          <h2 className="text-4xl font-semibold text-white mb-4">
+            Welcome back to Instacart
+          </h2>
+          <p className="text-white/60 font-serif text-xl max-w-sm mx-auto">
+            Fresh groceries and organic produce, delivered to your doorstep.
+          </p>
+        </div>
+      </div>
+
+      {/* Right Side */}
+      <div className="flex-1 flex-center px-4 py-12 bg-app-cream">
+        <div className="w-full max-w-md">
+
+          {/* form header message */}
+          <div className="text-center mb-8">
+
+            <Link to="/" className="inline-flex items-center gap-2 mb-6">
+              <BikeIcon className="size-8 text-app-screen"/>
+              <span className="text-2xl font-semibold text-app-green">Instacart</span>
+            </Link>
+
+            <h1 className="text-2xl font-semibold text-app-green mb-2">
+              {isLoginState?"Sign in to your account":"Sign up for an account"}
+            </h1>
+
+            <p className="text-sm text-app-text-light">
+              {isLoginState?"Don't have an account?":"Already have an account?"} 
+              <button onClick={()=>{setIsLoginState(!isLoginState)}} className="text-orange-500 ml-1 font-semibold hover:text-orange-600 transition-colors" >
+                {isLoginState?"Create one":"Sign in"}
+              </button>
+            </p>
+          </div>
+
+
+          {/* Login/Register From */}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
