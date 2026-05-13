ddac6a31a2a02f7b92ac516fe72d093e49118ea0
kashifashah1 <kashifa.shah24@gmail.com>
2026-05-13 13:46:15 +0530

    Working on navbar.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,17 @@
 import { createRoot } from 'react-dom/client'
+//This is used to connect React with the HTML page.
+
 import './index.css'
+//css will apply throughout the application.
+
 import App from './App.tsx'
+//This imports the main component of your application.
+//contains all pages/routes/components.
+
 import {BrowserRouter} from 'react-router-dom'
+//It enables routing/navigation in React.
+//React routes will not work.
+//BrowserRouter enables client-side routing in React applications. It allows navigation between pages without refreshing the browser by managing the URL using browser history API.
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
