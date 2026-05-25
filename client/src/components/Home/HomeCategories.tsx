5ae8a16b6548f92a7342c84cd6fd8ae1adee84c0
kashifashah1 <kashifa.shah24@gmail.com>
2026-05-25 14:01:27 +0530

    Working on Home Page

---
+++ client/src/components/Home/HomeCategories.tsx
@@ -0,0 +1,18 @@
+
+const HomeCategories = () => {
+  return (
+    <section className="py-16">
+        <div className="max-w-7xl mx-auto">
+            <div>
+                <h2 className="text-2xl font-semibold">Browse Categories</h2>
+                <p className="text-sm text-app-text-light mt-1">Find exactly what you need using</p>
+            </div>
+            <div className="flex items-center mt-8 overflow-x-scroll no-scrollbar">
+
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default HomeCategories
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,7 +1,14 @@
+import Features from "../components/Home/Features"
+import Hero from "../components/Home/Hero"
+import HomeCategories from "../components/Home/HomeCategories"
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className="min-h-screen max-w-7xl mx-auto px-4  sm:px-6 lg:px-8 py-12">
+      <Hero/>
+      <Features/>
+      <HomeCategories/>
+    </div>
   )
 }
 
